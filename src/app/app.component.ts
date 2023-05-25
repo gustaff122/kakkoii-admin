fefe158c1081b1610f088b51885271200c852bf1
gustaff122 <xseoos@gmail.com>
2023-05-26 00:21:09 +0200

    fix blinking thumbnails' loaders; add progression bar on routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { NgProgress, NgProgressRef } from 'ngx-progressbar';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ],
 })
-export class AppComponent {
-  title = 'KakkoiiAP';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription = new Subscription();
+  private progressRef: NgProgressRef;
+
+  constructor(
+    private readonly ngProgress: NgProgress,
+    private readonly router: Router,
+  ) {
+  }
+
+  public ngOnInit(): void {
+    this.progressRef = this.ngProgress.ref('routingProgress');
+
+    this.subscriptions.add(
+      this.router.events.subscribe((event) => {
+        switch (true) {
+          case event instanceof NavigationStart: {
+            this.progressRef.start();
+            break;
+          }
+
+          case event instanceof NavigationEnd:
+          case event instanceof NavigationCancel:
+          case event instanceof NavigationError: {
+            this.progressRef.complete();
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
--- src/app/ui/organisms/navbar/navbar.component.ts
+++ src/app/ui/organisms/navbar/navbar.component.ts
@@ -1,9 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { JwtService } from '@kakkoii/services/jwt.service';
-import { Subscription } from 'rxjs';
-import { NgProgress, NgProgressModule, NgProgressRef } from 'ngx-progressbar';
+import { NgProgressModule } from 'ngx-progressbar';
 
 @Component({
   selector: 'kk-navbar',
@@ -17,49 +16,15 @@ import { NgProgress, NgProgressModule, NgProgressRef } from 'ngx-progressbar';
   ],
   standalone: true,
 })
-export class NavbarComponent implements OnInit, OnDestroy {
-  private subscriptions: Subscription = new Subscription();
-  private progressRef: NgProgressRef;
-
-
+export class NavbarComponent {
   constructor(
     private readonly jwtService: JwtService,
     private readonly router: Router,
-    private readonly ngProgress: NgProgress,
   ) {
   }
 
   public logOut(): void {
     this.jwtService.removeToken();
     this.router.navigate([ '/auth' ]);
-  }
-
-  public ngOnInit(): void {
-    this.progressRef = this.ngProgress.ref('routingProgress');
-
-    this.subscriptions.add(
-      this.router.events.subscribe((event) => {
-        switch (true) {
-          case event instanceof NavigationStart: {
-            this.progressRef.start();
-            break;
-          }
-
-          case event instanceof NavigationEnd:
-          case event instanceof NavigationCancel:
-          case event instanceof NavigationError: {
-            this.progressRef.complete();
-            break;
-          }
-          default: {
-            break;
-          }
-        }
-      }),
-    );
-  }
-
-  public ngOnDestroy(): void {
-    this.subscriptions.unsubscribe();
   }
 }
